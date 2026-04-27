5d08a5c4e95b9a0db4faca1b55dac18696ff17be
Stylesproline <Stylesproline@mail.com>
2026-04-27 21:57:57 +0300

    Исправлена структура папок

--- app/api/products/route.ts
+++
@@ -1,7 +0,0 @@
-import { sql } from '@vercel/postgres';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  const { rows } = await sql`SELECT * FROM products ORDER BY id ASC`;
-  return NextResponse.json(rows);
-}
