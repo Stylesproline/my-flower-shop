7a45218952d05fc6feb9343642f12caeefb014a4
Stylesproline <Stylesproline@mail.com>
2026-04-29 21:53:24 +0300

    Add cart and dynamic logicmmxvzx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,26 @@
-import Script from 'next/script';
+import './globals.css'
+import Script from 'next/script'
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="ru">
-      <body>
-        {/* Вставляем обычным тегом для максимальной надежности */}
-        <script src="https://telegram.org" defer></script>
+      <head>
+        {/* Подключаем SDK через специальный компонент Next.js */}
+        <Script 
+          src="https://telegram.org" 
+          strategy="beforeInteractive" 
+        />
+      </head>
+      <body className="antialiased">
         {children}
       </body>
     </html>
-  );
+  )
 }
+
 
 
