f52c923ebc2e1fca94e0c96a1696d95064a81d9b
Stylesproline <Stylesproline@mail.com>
2026-05-07 16:01:56 +0300

    feat: add product images Flowww

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const PRODUCTS = [
   { id: 3, category: 'Букеты', name: 'Розы Лилии Микс', price: 299, desc: 'Цветочная подписка 1 раз в неделю/на 1 месяц', image: '/images/lili-rose.jpeg' },
   { id: 4, category: 'Розы', name: 'Белый Шоколад', price: 89 , desc: 'Белоснежные розы высшего сорта/10 шт. ', image: '/images/field-dream.jpg' },
   { id: 5, category: 'Пионы', name: 'Пион белый ', price: 87, desc: 'Пион белый 9 шт.', image: '/images/yquued.jpeg' },
-  { id: 3, category: 'Букеты', name: 'Хризантемы одноголовые', price: 57, desc: 'Хризантемы 9 шт.', image: '/images/77ntitled.jpeg' },
+  { id: 6, category: 'Букеты', name: 'Хризантемы одноголовые', price: 57, desc: 'Хризантемы 9 шт.', image: '/images/77ntitled.jpeg' },
 ];
 
 const CATEGORIES = ['Все', 'Розы', 'Пионы', 'Букеты'];
